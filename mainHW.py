83359bdebe1cc487f55d347b02b7aefbfec254ee
Alexander Sukhetskiy <alexander.suhetskiy@gmail.com>
2021-07-31 14:45:36 +0300

    add new classes

--- mainHW.py
+++ mainHW.py
@@ -21,15 +21,25 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+class Lecturer():
+    pass
+
+class Reviewer():
+    pass
+
+def main():
+    best_student = Student('Ruoy', 'Eman', 'your_gender')
+    best_student.courses_in_progress += ['Python']
  
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+    cool_mentor = Mentor('Some', 'Buddy')
+    cool_mentor.courses_attached += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+    cool_mentor.rate_hw(best_student, 'Python', 10)
+    cool_mentor.rate_hw(best_student, 'Python', 10)
+    cool_mentor.rate_hw(best_student, 'Python', 10)
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+    print(best_student.grades)
+
+if __name__ == "__main__":
+  main()
