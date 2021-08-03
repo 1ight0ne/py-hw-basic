a13811330ea53dc74c0b21bfa2a7526d085a7ba0
Alexander Sukhetskiy <alexander.suhetskiy@gmail.com>
2021-08-03 15:41:03 +0300

    files task2 complete

--- files/filesHW.py
+++ files/filesHW.py
@@ -19,5 +19,20 @@ def read_cookbook():
             f.readline()
     return cook_book
 
+def get_shop_list_by_dishes(dishes, person_count):
+    result_dict = {}
+    for dish_name in dishes:
+        if dish_name in cook_book:
+            for ingrs in cook_book[dish_name]:
+                if ingrs['ingredient_name'] not in result_dict:
+                    result_dict[ingrs['ingredient_name']] = {'measure': ingrs['measure'], 'quantity': ingrs['quantity'] * person_count}
+                else:
+                    result_dict[ingrs['ingredient_name']]['quantity'] += ingrs['quantity'] * person_count
+        else:
+            print(f'Блюдо {dish_name} отсутствует в списке блюд.')
+    return result_dict
+
 if __name__ == '__main__':
-    print(read_cookbook())
+    cook_book = read_cookbook()
+    print(cook_book)
+    pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет', 'Окрошка'], 5))
