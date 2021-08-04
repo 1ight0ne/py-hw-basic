b7da7610b51b1f9853dd6f6147d868c1c5381eeb
Alexander Sukhetskiy <alexander.suhetskiy@gmail.com>
2021-08-04 17:33:06 +0300

    task3 complete

--- files/filesHW.py
+++ files/filesHW.py
@@ -32,7 +32,32 @@ def get_shop_list_by_dishes(dishes, person_count):
             print(f'Блюдо {dish_name} отсутствует в списке блюд.')
     return result_dict
 
+def concat_files(files_count, *files):
+    if len(files) == files_count and files_count > 1:
+        if not os.path.exists(os.getcwd() + '\\sorted'):
+            return 'Ошибка! папка sorted не существует'
+        os.chdir('sorted')
+        files = list(map(lambda f: os.path.join(os.getcwd(), f), files))
+        output_file = os.path.join(os.getcwd(), 'out_sorted.txt')
+        files_to_dict = []
+        for file in files:
+            if not os.path.exists(file):
+                return f'Ошибка! файл {file} не существует'
+            with open(file, 'r', encoding='utf-8') as f:
+                content = f.readlines()
+            files_to_dict.append({'filename': os.path.basename(file), 'content': content, 'lines_count':len(content)})
+        files_to_dict.sort(key=lambda e: e['lines_count'])
+        with open(output_file, 'w', encoding='utf-8') as f:
+            for file in files_to_dict:
+                f.write(file['filename'] + '\n')
+                f.write(str(file['lines_count']) + '\n')
+                f.writelines(file['content'] )
+        return 'Успех!'
+    else:
+        return f'Ошибка! указано неверное количество фалов'
+
 if __name__ == '__main__':
     cook_book = read_cookbook()
     print(cook_book)
     pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет', 'Окрошка'], 5))
+    print(concat_files(3,'1.txt', '2.txt', '3.txt'))
